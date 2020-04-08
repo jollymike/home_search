6c5d8222a788a025cc1efdc8233eb6ae8de0b54e
jollymike <jollymike@gmail.com>
2020-04-08 13:05:58 -0400

    added data files; updated lon to lng; created output dir;

--- find_next_home.py
+++ find_next_home.py
@@ -23,9 +23,15 @@ def get_response(api,params):
         print('The request did not time out')        
     return response
 
-def get_places(lat,lon,radius,keyword):
+def get_places(lat,lng,radius,keyword):
     API = r'nearbysearch/'
-    PARAMS = f"location={lat},{lon}&radius={radius}&keyword={keyword}"
+    PARAMS = f"location={lat},{lng}&radius={radius}&keyword={keyword}"
+    response = get_response(API,PARAMS)
+    return response
+
+def get_next_pg(next_page_token):
+    API = r'nearbysearch/'
+    PARAMS = f"next_page_token={next_page_token}"
     response = get_response(API,PARAMS)
     return response
 
@@ -39,7 +45,7 @@ def stringClean(string):
     string = string.strip().replace(" ", "-").replace("\\", "-").replace(".", "").replace("/", "-")
     return string
 
-MARTA_STATIONS = r'C:\Users\MLondeen\data\marta_stations.csv'
+MARTA_STATIONS = r'.\data\marta_stations_test.csv'
 
 # get list of dictionaries with some keys from the original request and some keys from a secondary request
 
@@ -54,27 +60,21 @@ with open(MARTA_STATIONS) as csv_file:
         s = row['Station']
         print(f'Retrieving apartments near Station {s}..')
         sleep(1) # make sure the calls are nice and slow
-        response = get_places(row['lat'],row['lon'],'900','apartment')
-
+        response = get_places(row['lat'],row['lng'],'900','apartment')
         if response:
             results = response.json()
             print(f'{s} returned {len(results)} results. Attempting to process..')
             for result in results['results']:
                 n = result['name']
-                print(f'Retrieving details for {n}..')
-                
+                print(f'Retrieving details for {n}..')                
                 coords_station = (row['lat'],row['lng'])
-                coords_apt =     (result['geometry']['location']['lat'],result['geometry']['location']['lng'])
-
-                print geopy.distance.vincenty(coords_1, coords_2).km
-
+                coords_apt =     (result['geometry']['location']['lat'],result['geometry']['location']['lng'])                
                 a = {'place_id':result['place_id'],
                     'name':result['name'],
                     'lat':result['geometry']['location']['lat'],
                     'lng':result['geometry']['location']['lng'],
                     'nearest_station':row['Station'],
-                    'distance_to_station':geopy.distance.distance(coords_station,coords_apt).miles}
-                
+                    'distance_to_station':gd.distance(coords_station,coords_apt).miles}                
                 sleep(1) # make sure the calls are nice and slow
                 details = get_details(result['place_id'])
                 if details:
@@ -84,14 +84,14 @@ with open(MARTA_STATIONS) as csv_file:
                     result_list.append(c)
                     total_ct += 1
                 else:
-                    result_list.append(a)
-        
+                    result_list.append(a)        
             print(f'Processing for {s} complete. Persisting to Disk..')
             out_df = pd.DataFrame(result_list)
             out_df.to_csv(stringClean(f'station_{s}_apartments.csv'), index=False)
-            print(f'Finished! Moving on to next set..')
-        
+            print(f'Finished! Moving on to next set..')        
         else:
-            print(f'Request Failed! Moving on to next set..')
-        
+            print(f'Request Failed! Moving on to next set..')        
+
+
 print(f'Fetched a total of {total_ct} results. Shutting Down..')
+
