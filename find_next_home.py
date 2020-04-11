deaeae6f422b1e4ecb09421029d492a7d01233b6
jollymike <jollymike@gmail.com>
2020-04-11 16:13:16 -0400

    refactored and cleaned code; new process_response f(x); can now handle multiple pages from API;

--- find_next_home.py
+++ find_next_home.py
@@ -1,7 +1,7 @@
 import csv
-import requests
 import pandas as pd
 from time import sleep
+from requests import get
 from requests.exceptions import Timeout
 import geopy.distance as gd
 
@@ -15,82 +15,85 @@ def get_response(api,params):
     CALL = f"{BASE_URL}{api}{OUTPUT_TYPE}?key={API_KEY}&{params}"
     print(f"Attempting call to {CALL}")
     try:
-        response = requests.get(CALL, timeout=(5,10))
+        response = get(CALL, timeout=(5,10))
     except Timeout:
         print('The request timed out')
         response = False
     else:
         print('The request did not time out')        
-    return response
+        return response
 
 def get_places(lat,lng,radius,keyword):
     API = r'nearbysearch/'
     PARAMS = f"location={lat},{lng}&radius={radius}&keyword={keyword}"
-    response = get_response(API,PARAMS)
-    return response
+    return get_response(API,PARAMS)
 
 def get_next_pg(next_page_token):
     API = r'nearbysearch/'
     PARAMS = f"next_page_token={next_page_token}"
-    response = get_response(API,PARAMS)
-    return response
+    return get_response(API,PARAMS)
 
 def get_details(place_id):
     API = r'details/'
     PARAMS = f"place_id={place_id}&fields=name,rating,formatted_phone_number,formatted_address,url,website,price_level"
-    response = get_response(API,PARAMS)
-    return response
+    return get_response(API,PARAMS)
 
 def stringClean(string):
-    string = string.strip().replace(" ", "-").replace("\\", "-").replace(".", "").replace("/", "-")
-    return string
+    return string.strip().replace(" ", "-").replace("\\", "-").replace(".", "").replace("/", "-")
+     
+def process_response(response):
+    print(f'This station has {len(response.json())} nearby apartments. Attempting to process..')
+    result_list = []
+    for result in response.json()['results']:                
+        coords_station = (row['lat'],row['lng'])
+        coords_apt     = (result['geometry']['location']['lat'],result['geometry']['location']['lng'])                
+        apt = {'place_id':result['place_id'],
+               'name':result['name'],
+               'lat':result['geometry']['location']['lat'],
+               'lng':result['geometry']['location']['lng'],
+               'nearest_station':row['Station'],
+               'distance_to_station':gd.distance(coords_station,coords_apt).miles}                
+        sleep(.1) # make sure the calls are nice and slow
+        details = get_details(result['place_id'].json()['result'])
+        if details:
+            result_list.append({**apt,**details})
+            total_ct += 1
+        else:
+            result_list.append(apt)
+    return result_list
+        
+
+def build_request(input):
+
+def persist_data(data,station):
+    print('Processing for station complete. Persisting to Disk..')
+    pd.DataFrame(data).to_csv(stringClean(f'station_{station}_apartments.csv'), index=False)
 
 MARTA_STATIONS = r'.\data\marta_stations_test.csv'
 
-# get list of dictionaries with some keys from the original request and some keys from a secondary request
-
 total_ct = 0
 
+# driver
 with open(MARTA_STATIONS) as csv_file:
     csv_reader = csv.DictReader(csv_file, delimiter=',')
     line_count = 0
     print(f'Data loaded..')
     for row in csv_reader:
-        result_list = []
-        s = row['Station']
-        print(f'Retrieving apartments near Station {s}..')
-        sleep(1) # make sure the calls are nice and slow
+        station = row['Station']
+        print(f'Retrieving apartments near Station {station}..')
+        sleep(.1) # make sure the calls are nice and slow
         response = get_places(row['lat'],row['lng'],'900','apartment')
+        apartment_list = []
         if response:
-            results = response.json()
-            print(f'{s} returned {len(results)} results. Attempting to process..')
-            for result in results['results']:
-                n = result['name']
-                print(f'Retrieving details for {n}..')                
-                coords_station = (row['lat'],row['lng'])
-                coords_apt =     (result['geometry']['location']['lat'],result['geometry']['location']['lng'])                
-                a = {'place_id':result['place_id'],
-                    'name':result['name'],
-                    'lat':result['geometry']['location']['lat'],
-                    'lng':result['geometry']['location']['lng'],
-                    'nearest_station':row['Station'],
-                    'distance_to_station':gd.distance(coords_station,coords_apt).miles}                
-                sleep(1) # make sure the calls are nice and slow
-                details = get_details(result['place_id'])
-                if details:
-                    b = details.json()
-                    b = b['result']
-                    c = {**a,**b}
-                    result_list.append(c)
-                    total_ct += 1
-                else:
-                    result_list.append(a)        
-            print(f'Processing for {s} complete. Persisting to Disk..')
-            out_df = pd.DataFrame(result_list)
-            out_df.to_csv(stringClean(f'station_{s}_apartments.csv'), index=False)
-            print(f'Finished! Moving on to next set..')        
+            apartment_list = apartment_list**, process_response(response)**)
+            if 'next_page_token' in response.json():
+                response = get_places(row['lat'],row['lng'],'900','apartment')
+                process_response(response)
+            else:
+                print(f'Finished current station Moving on to next station..')
+            persist_data(apartment_list,station)
         else:
-            print(f'Request Failed! Moving on to next set..')        
+            print(f'Request Failed! Moving on to next station..')        
 
 
 print(f'Fetched a total of {total_ct} results. Shutting Down..')
